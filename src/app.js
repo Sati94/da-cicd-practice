c9f3b09bfbb178808b3a6b3664dfc0e096f70cb4
Ati <nagyonkellvalamihez@gmail.com>
2024-10-01 19:17:11 +0200

    recipe list is ready, we can see the recipes

--- src/app.js
+++ src/app.js
@@ -22,29 +22,77 @@ function element(tag, attributes = {}, children = []) {
   })
   return element;
 }
+function createRecipeCard(recipe) {
+  const card = document.createElement('div');
+  card.className = "card mb-3 col-md-4'";
+  card.style = 'max-width: 300px';
+
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  const cardTitle = document.createElement('h5');
+  cardTitle.className = 'card-title';
+  cardTitle.textContent = recipe.name;
+
+  const ingredientList = document.createElement('ul');
+  ingredientList.className = 'list-group list-group-flush';
+
+  recipe.ingredients.forEach(ingredient => {
+    const listItem = document.createElement('il');
+    listItem.className = 'list-group-item';
+    listItem.textContent = `${ingredient.quantity} ${ingredient.item}`;
+    ingredientList.appendChild(listItem);
+  });
+
+  const instructionsTitle = document.createElement('h6');
+  instructionsTitle.className = 'mt-3';
+  instructionsTitle.textContent = 'Instructions:';
+
+  const instructionsList = document.createElement('ol');
+  recipe.instructions.forEach(instruction => {
+    const listItem = document.createElement('li');
+    listItem.textContent = instruction;
+    instructionsList.appendChild(listItem);
+  });
+
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(ingredientList);
+  cardBody.appendChild(instructionsTitle);
+  cardBody.appendChild(instructionsList);
+  card.appendChild(cardBody);
+
+  return card;
+}
 
 function createContainer({ onShow }) {
   const container = element('div', { class: 'container' }, [
     element('h1', {}, ['My Recipes']),
     element('button', { class: 'btn btn-primary', onClick: onShow }, ['Show Recipes']),
-    element('div', { id: 'recipeList' }),
+    element('div', { id: 'recipeList', class: 'rows' }),
   ])
   return container;
 }
 
 export function setupApp(root) {
-  
+
   let isVisible = false;
-  
+
   function handleShow(event) {
     isVisible = !isVisible;
     const list = event.target.parentNode.querySelector('#recipeList');
-    
-    
-    if (isVisible) {      
-      list?.appendChild(element('p', {}, ['Recipe List']));
-    } else {
-      list.innerText = '';
+
+
+    if (isVisible) {
+      const heading = document.createElement('h2');
+      heading.textContent = 'Recipe List';
+      list.appendChild(heading);
+
+      const recipes = getRecipes();
+      recipes.forEach(recipe => {
+        const recipeCard = createRecipeCard(recipe);
+        list.appendChild(recipeCard);
+      })
     }
   }
 
